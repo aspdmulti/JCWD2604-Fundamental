88b0e315a7a0f46775d984a7597c6f3db6b0c627
aspdmulti <anggadwikrishnajaya@gmail.com>
2023-11-29 10:55:04 +0700

    updated

--- day 6/exercisejordan.js
+++ day 6/exercisejordan.js
@@ -48,3 +48,72 @@ console.log(ACDC("descending", [5, 4, 3, 1, 2]));
 console.log(ACDC("asal", [5, 4, 3, 2, 1]));
 
 //5
+function add(arrn, newel) {
+  let cek = arrn.includes(newel);
+  if (cek == false) {
+    arrn.push(newel);
+  }
+  return arrn;
+}
+console.log(add([1, 2, 3, 4, 5], 7));
+
+//6
+function sumnih(arr1, arr2) {
+  return arr1.concat(arr2).reduce((go, curr) => go + curr);
+}
+console.log(sumnih([1, 2], [3, 4]));
+
+//7
+let contoh = [1, 2, 3, [4, 5, [6, 7, 8]]];
+function dearr(arr) {
+  arr.forEach((val) => {
+    if (!Array.isArray(val)) {
+      console.log(val);
+    } else {
+      val.forEach((val2) => {
+        if (!Array.isArray(val2)) {
+          console.log(val2);
+        } else {
+          val2.forEach((val3) => {
+            console.log(val3);
+          });
+        }
+      });
+    }
+  });
+}
+dearr(contoh);
+//8
+function ascii(string = "") {
+  let numbersum = 0;
+  for (let i = 0; i < string.length; i++) {
+    numbersum += string.charCodeAt(i);
+  }
+  return numbersum;
+}
+console.log(ascii("aa"));
+
+//9
+const rec = (n, total = 0) => {
+  if (n > 0) return rec(n - 1, (total += n));
+  return total;
+};
+console.log(rec(5));
+
+//10
+function searchi(arr, x) {
+  const r = [];
+  arr.map((val, i) => {
+    if (val == x) {
+      r.push(i);
+    }
+  });
+  return r;
+}
+console.log(searchi([1, 2, 3, 4, 2, 3, 2], 2));
+
+//11
+function oe(...arr) {
+  return arr.map((val) => (val % 2 == 0 ? "genap" : "ganjil"));
+}
+console.log(oe(1, 2, 3, 4));
